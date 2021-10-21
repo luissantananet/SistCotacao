9842085f985dce99e1e8b88689f5bff380f3cd88
luissantananet <53272578+luissantananet@users.noreply.github.com>
2021-10-21 07:20:18 -0300

    Update datebase_cotacao.sql

--- datebase_cotacao.sql
+++ datebase_cotacao.sql
@@ -3,78 +3,20 @@ use cotacao;
 
 create table tarifa(
 	id int not null auto_increment,
-    tarifa_base decimal,
-    ad_valoren decimal,
-    gris decimal,
-    taxa decimal,
-    icms decimal,
-    f_cif decimal,
-    f_fob decimal,
-    flitoral decimal,
+    frete_peso decimal(7,2),
+    pedagio decimal(7,2),
+    ad_valoren decimal(7,2),
+    gris decimal(7,2),
+    taxa decimal(7,2),
+    icms decimal(7,2),
+    f_cif decimal(7,2),
+    f_fob decimal(7,2),
+    flitoral decimal(7,2),
     primary key(id)
 );
-create table tarifa_20(
+create table tarifas_minimas(
 	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_50(
-	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_100(
-	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_150(
-	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_200(
-	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_250(
-	id int not null auto_increment,
-    tarifa_base decimal,
-    gris decimal,
-    ffinal decimal,
-    ad_valoren decimal,
-    ftotal decimal,
-    flitoral decimal,
-    primary key(id)
-);
-create table tarifa_300(
-	id int not null auto_increment,
+    descricao varchar(50),
     tarifa_base decimal,
     gris decimal,
     ffinal decimal,
@@ -93,12 +35,12 @@ create table cotacao(
     cidade_destino varchar(100),
     estado_destino varchar(100),    
     tipo varchar(3),
-    valor_merc decimal,
-    peso decimal,
+    valor_merc decimal(10,2),
+    peso decimal(10,3),
     volume int,
     tipo_merc varchar(100),
-    peso_cubo_total decimal,
-    m3_total decimal,
+    peso_cubo_total decimal(10,3),
+    m3_total decimal(10,3),
     primary key(id)
 );
 create table cubagem(
